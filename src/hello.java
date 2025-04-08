1f21414260065cfe097df6915554e6a2b4f8dc68
Harsh Wathrey <harsh12wathrey@gmail.com>
2025-04-08 13:46:40 +0530

    msg

---
+++ src/hello.java
@@ -0,0 +1,13 @@
+package src;
+
+public class hello {
+    public static void main(String[] args) {
+//        int i;
+//        for (i = 0; i<3; i++){
+//            System.out.println("8");
+        int b = 119;
+        byte a =(byte) b;
+        System.out.println(a);
+    }
+
+}
