95576e092b499f104e8896a878cf21e83f76421a
HarshWathrey <harsh12wathrey@gmail.com>
2025-10-11 22:46:53 +0530

    JAVA

---
+++ python/day1.py
@@ -0,0 +1,3 @@
+import keyword
+print(keyword.kwlist)
+# print("Total no. of kewwords", len keyword.kwlist;)2
---
+++ python/day2.py
@@ -0,0 +1,12 @@
+a = 10
+b= 20
+c= 30
+
+if (a>=b) and (a>=c):
+    Largest = a
+elif (b>=c) and (b>=c):
+    Largest = b
+else:
+    Largest = c
+
+print("Largest Num is:-", Largest)
